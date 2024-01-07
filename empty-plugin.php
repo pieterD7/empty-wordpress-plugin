cf61546165bd9195daa031b253f23310ae060c48
pieter <pieterhoekstra@gmail.com>
2024-01-07 23:02:40 +0100

    Revert "First demo"
    This reverts commit b870c21aa9d62bde3b6ac5e95ea078f04a433f97.

--- empty-plugin.php
+++ empty-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: WCAG Made easy
-Description: Accessibility guidelines made easy. Inclusive websites and WCAG compattability in a breeze!
+Plugin Name: Empty Plugin
+Description: Empty WordPress plugin with settings page
 Version:     1.0.0
 Author:      Pieter Hoekstra
 License:     GPL2
@@ -10,29 +10,23 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 defined( 'ABSPATH' ) or die( 'Nope, not accessing this' );
 
-define( 'WCAGME_PLUGIN_DIR', __DIR__ );
+define( 'EMPTY_PLUGIN_DIR', __DIR__ );
 
-define( 'WCAGME_PLUGIN_FOLDERNAME', basename( __DIR__ ) );
+define( 'EMPTY_PLUGIN_PLUGIN_FILE', __FILE__ );
 
-define( 'WCAGME_PLUGIN_FILE', __FILE__ );
+define( 'EMPTY_PLUGIN_TEXT_DOMAIN', 'empty-plugin' );
 
-define( 'WCAGME_PLUGIN_TEXT_DOMAIN', 'wcagme-textdomain' );
+function autoloader_empty_plugin( $class_name ){
 
-define( 'WCAGME_TEXT_DOMAIN', WCAGME_PLUGIN_TEXT_DOMAIN );
+	$class_name = str_replace( '\\', '/', $class_name );
 
-function autoloader_wcagme_plugin( $class_name ){
-
-	$class_name = str_replace( '\\', DIRECTORY_SEPARATOR, $class_name );
-
-	include_once WCAGME_PLUGIN_DIR . DIRECTORY_SEPARATOR .  ".." . 
-		DIRECTORY_SEPARATOR . WCAGME_PLUGIN_FOLDERNAME . DIRECTORY_SEPARATOR 
-		. $class_name . '.php';
+	include_once EMPTY_PLUGIN_DIR . "/" . $class_name . '.php';
 }
 
-spl_autoload_register( 'autoloader_wcagme_plugin');
+spl_autoload_register( 'autoloader_empty_plugin');
 
-new WCAGME\Application;
+new Application\Application;
 
-spl_autoload_unregister( 'autoloader_wcagme_plugin' );
+spl_autoload_unregister( 'autoloader_empty_plugin' );
 
 ?>
